586f9f19f8ed423375aaf0e95d7ebf3bd9acac72
SlyVL <harrisonmuldowney06@gmail.com>
2025-05-12 10:34:59 +0100

    Player Movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] Rigidbody rb;
+    Vector3 moveInput;
+    [SerializeField] float MovementMultiplier;
+
+
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+        
+    }
+
+    public void OnMove(InputAction.CallbackContext MoveValue)
+    {
+        var tempvar = MoveValue.ReadValue<Vector2>();
+        moveInput = new Vector3(tempvar.x, 0, tempvar.y);
+        
+    }
+
+    public void PlayerMove()
+    {
+        rb.AddForce(moveInput * MovementMultiplier);
+    }
+}
