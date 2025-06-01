4b1c8972a5514b4b3678bfddb0d463cde34a95d1
SlyVL <harrisonmuldowney06@gmail.com>
2025-06-01 20:04:10 +0100

    Added checkpoints and timer

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public int checkpointIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        TimeTrialManager manager = FindObjectOfType<TimeTrialManager>();
+
+        // If first checkpoint, start race
+        if (checkpointIndex == 0 && !manager.IsRaceStarted())
+        {
+            manager.StartRace();
+        }
+
+        // Notify the manager this checkpoint was hit
+        manager.HitCheckpoint(this);
+    }
+}
+
+
---
+++ Assets/Scripts/TimeTrialManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeTrialManager : MonoBehaviour
+{
+    public Transform[] checkpoints;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI finalTimeText;
+    private int currentCheckpoint = 0;
+    private float timer = 0f;
+    private bool raceStarted = false;
+    private bool raceFinished = false;
+
+    void Update()
+    {
+        if (raceStarted && !raceFinished)
+        {
+            timer += Time.deltaTime;
+            UpdateTimerUI();
+        }
+    }
+
+    public void StartRace()
+    {
+        timer = 0f;
+        currentCheckpoint = 0;
+        raceStarted = true;
+        raceFinished = false;
+        finalTimeText.gameObject.SetActive(false);
+    }
+
+    public void HitCheckpoint(Checkpoint checkpoint)
+    {
+        if (!raceStarted) return;
+
+        if (checkpoint.checkpointIndex == currentCheckpoint)
+        {
+            currentCheckpoint++;
+            if (currentCheckpoint >= checkpoints.Length)
+            {
+                FinishRace();
+            }
+        }
+    }
+
+    public bool IsRaceStarted()
+    {
+        return raceStarted;
+    }
+
+
+
+
+    void FinishRace()
+    {
+        raceFinished = true;
+        raceStarted = false;
+        finalTimeText.text = "Final Time: " + timer.ToString("F2") + "s";
+        finalTimeText.gameObject.SetActive(true);
+    }
+
+    void UpdateTimerUI()
+    {
+        timerText.text = timer.ToString("F2") + "s";
+    }
+}
